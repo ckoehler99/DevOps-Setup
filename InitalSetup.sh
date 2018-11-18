fcb9e71819bd294b42fff8dc27c31a882ead4012
Christian Köhler <ckoehler99@gmail.com>
2018-11-18 17:31:37 +0100

    Update InitalSetup.sh

--- InitalSetup.sh
+++ InitalSetup.sh
@@ -4,7 +4,13 @@ echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/source
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update && apt-get upgrade -y
-sudo apt install -y openjdk-8-jre jenkins nginx python-certbot-nginx libssl-dev libffi-dev python-dev python-pip docker.io git
+sudo apt install -y openjdk-8-jre jenkins nginx python-certbot-nginx libssl-dev libffi-dev python-dev python-pip docker.io git curl
+# jfrog artifactory
+echo "deb https://jfrog.bintray.com/artifactory-debs xenial main" | sudo tee -a /etc/apt/sources.list
+curl https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | sudo apt-key add -
+sudo apt-get update
+sudo apt install -y jfrog-artifactory-oss 
+
 sudo ufw allow OpenSSH
 sudo ufw allow 'Nginx Full'
 sudo ufw enable
